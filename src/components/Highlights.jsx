c8a90ee92e0c517697c3c23312009222eb86b196
Pepela John <pepelajahy@gmail.com>
2024-04-15 18:36:41 +0300

    feature highlight section start before the video carousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { watchImg, rightImg } from "../utils";
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", {
+      opacity: 1,
+      delay: 1.2,
+      stagger: "0.25",
+      y: 0,
+    });
+  }, []);
   return (
-    <div>Highlights</div>
-  )
-}
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc-900"
+    >
+      <div className="screen-max-width ">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img className="ml-2" src={watchImg} alt="play" />{" "}
+            </p>
+            <p className="link">
+              Watch the event
+              <img className="ml-2" src={rightImg} alt="right" />{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Highlights
+export default Highlights;
